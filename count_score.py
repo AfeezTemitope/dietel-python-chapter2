750ac5982a5bd263d4382a54d93713c5b30e7153
Tbelzbby <belloafeez28@gmail.com>
2024-04-12 12:23:47 +0100

    commit

--- count_score.py
+++ count_score.py
@@ -1,9 +1,9 @@
 
-    pass_count = 0
-    fail_count = 0
+pass_count = 0
+fail_count = 0
 
    
-    for i in range(1, 16):
+for i in range(1, 16):
         try:
             score = float(input(f"Enter score for student {i}: "))
             if score >= 45:
@@ -14,5 +14,5 @@
             print("Invalid input. Please enter a valid numeric score.")
 
  
-    print(f"Number of students who passed: {pass_count}")
-    print(f"Number of students who failed: {fail_count}")
+print(f"Number of students who passed: {pass_count}")
+print(f"Number of students who failed: {fail_count}")
