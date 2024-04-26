58ef32198b1e82fc5aa1a192a6aa67f5c95ca229
Tbelzbby <belloafeez28@gmail.com>
2024-04-26 15:00:13 +0100

    commit

---
+++ classWork/find_minimum.py
@@ -0,0 +1,9 @@
+def minimum(number):
+	if not number:
+		return None
+
+	minimum = number[0]
+	for num in number:
+		if num < minimum:
+			minimum = num
+	return minimum
---
+++ classWork/test_find_minimum.py
@@ -0,0 +1,4 @@
+from find_minimum import minimum
+
+def test_find_minimum():
+	assert minimum([2,3,45,6,77,6,55,4,34])
